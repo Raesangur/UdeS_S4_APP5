0cebbc6bbe905766a1989c2132280ee532717890
Jacty Saenz <saej3101@usherbrooke.ca>
2022-07-12 14:58:37 -0400

    Correction affichage d'erreurs

--- code/src/app5/AnalLexErreur.java
+++ code/src/app5/AnalLexErreur.java
@@ -2,7 +2,7 @@ package app5;
 
 public class AnalLexErreur extends Exception {
     public AnalLexErreur(String msg, String s, int pointeur) {
-        super("Erreur Analyse Lexicale: " + msg + '\n' + s + '\n' + " ".repeat(pointeur) + "^");
+        super("Erreur Analyse Lexicale: " + msg + '\n' + s + '\n' + " ".repeat(pointeur - 1) + "^");
         
     }
 }
